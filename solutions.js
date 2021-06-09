a383bad9543a156004c185d5b8ac94c41b6edc3e
Kamrun Foster <kamrun.foster4@gmail.com>
2021-06-09 18:15:28 -0500

    doingJavaScriptRightNow() completed

--- solutions.js
+++ solutions.js
@@ -1,2 +1,23 @@
 // Write your Solutions here!!
+function doingJavaScriptRightNow(){
+    return true;
+}; // completed
 
+
+
+
+function isPositive(value){// complete
+    if (value > 0){
+        return true;
+    } else {
+        return false;
+    }
+} // complete # 6
+
+function isNegative(value) {// completed
+    if (value < 0) {
+        return true;
+    } else {
+        return false;
+    }
+} // complete # 7
