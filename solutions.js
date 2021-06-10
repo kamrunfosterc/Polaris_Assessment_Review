645f3f5ff8cf21dcea3195521d27e406ca71f8da
Kamrun Foster <kamrun.foster4@gmail.com>
2021-06-09 21:25:06 -0500

    updated

--- solutions.js
+++ solutions.js
@@ -8,13 +8,36 @@ function onMarsRightNow(){
 }// #2 completed
 
 function sayHello(value){
-    if (value === 'string' && 'undefined'){
-        return "Hello, " + value.toString() + "!";
-    } else {
-        return "Hello, world!";
-    }
-} //#3 partially not passing "cook" for some reason
+    if (typeof name === "string") { //could also make (typeof name === "string" && isNaN(name))
+        if(isNaN(name)){ //nested if statement
+            return "Hello, " + name + "!";
+        }
+        return "Hello, world!";// might be good with out
 
+    } return "Hello, world!";
+}
+//     if (value === 'string'){
+//         return "Hello, " + value + "!";
+//     } else {
+//         return "Hello, world!";
+//     }
+// } //#3 partially not passing "cook" for some reason
+
+function plusTwo(numb){
+    var new_numb = Number(numb) + 2;
+    if (numb === 'number'){
+        return new_numb;
+    } else if (numb == 'undefined'){
+        return NaN;
+    }else {
+        return NaN;
+    }
+
+}
+function addOne(value){
+    return Number(value) + 1;
+    // return ++value;
+}
 
 
 
@@ -33,3 +56,25 @@ function isNegative(value) {// completed
         return false;
     }
 } // #7 progress
+
+function isNumeric(i){
+    if(i === Number(i)){
+        return true;
+    } else{
+        return NaN;
+    }
+
+}// #8 working
+
+function isNotNumeric(i){
+    if(i !== Number(i)){
+        return false;
+    } else{
+        return NaN;
+    }
+
+}// #8 working
+
+function isVowel(par){
+    // some code
+}
